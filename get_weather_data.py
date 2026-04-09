3c4a718f83ad5fe6047f55a590f7da9f6faf4017
swaroop-khan <funkygamers10@gmail.com>
2026-04-09 18:59:42 +0530

    cleaned up code

--- get_weather_data.py
+++ get_weather_data.py
@@ -1,18 +1,19 @@
 import requests
-
-city_name=input("Enter city name: ")
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+city_name = input("Enter city name: ")
 API_Key = os.getenv('API_KEY')
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_Key}&units=metric'
 
-response=requests.get(url)
+response = requests.get(url)
 
-if response.status_code==200:
-    data=response.json()
-    weather_description=data['weather'][0]['description']
+if response.status_code == 200:
+    data = response.json()
+    weather_description = data['weather'][0]['description']
+
     if 'rain' in weather_description:
         icon = '🌧️'
     elif 'cloud' in weather_description:
@@ -23,9 +24,10 @@ if response.status_code==200:
         icon = '❄️'
     else:
         icon = '🌡️'
-    print(f"City 📍: {data['name']}")
-    print(f"Weather is: {data['weather'][0]['description']}")
-    print(f"Current Temperature 🌡️ : {data['main']['temp']}°C")
-    print(f"Humidity is: {data['main']['humidity']}%")
+
+    print(f"City: 📍 {data['name']}")
+    print(f"Weather is: {icon} {weather_description}")
+    print(f"Current Temperature: 🌡️ {data['main']['temp']}°C")
+    print(f"Humidity: 💧 {data['main']['humidity']}%")
 else:
-    print(f"City not found💭; Status code: {response.status_code}")
+    print(f"❌ City not found! Status code: {response.status_code}")
